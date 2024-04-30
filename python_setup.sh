b78e1d0dd00495a633f33a5e3a5ba08cf85c8770
hauntsaninja <hauntsaninja@gmail.com>
2024-04-29 23:40:19 -0700

    .

--- python_setup.sh
+++ python_setup.sh
@@ -82,6 +82,25 @@ Create a shell with arch -${ARCH_EXPECTED} zsh and try again \
             return 1
         fi
         $BREW install openssl readline sqlite3 xz zlib ncurses
+    elif [ "$(uname)" = "Linux" ]; then
+        apt-get update
+        apt-get install -y --no-install-recommends \
+            build-essential \
+            gdb \
+            lcov \
+            libbz2-dev \
+            libffi-dev \
+            libgdbm-dev \
+            liblzma-dev \
+            libncurses5-dev \
+            libreadline6-dev \
+            libsqlite3-dev \
+            libssl-dev \
+            lzma \
+            lzma-dev \
+            tk-dev \
+            uuid-dev \
+            zlib1g-dev
     fi
 
     # ------------------------------
